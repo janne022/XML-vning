aa4e9cf06b2137f9152c28720ae3e33b45648800
janne022 <jannis020609@gmail.com>
2020-09-23 11:08:01 +0200

    Add project files.

---
+++ XML övning/Pets.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XML_övning
+{
+    public class Pets
+    {
+        public string petName;
+        public string petDesc;
+        public int petSpeed;
+        int petLifeSpan = new LifeSpan().LifeSpanGenerator();
+    }
+    class LifeSpan
+    {
+        public int LifeSpanGenerator()
+        {
+            Random generator = new Random();
+            int lifeSpan = generator.Next(60,4321);
+            return lifeSpan;
+        }
+    }
+}
---
+++ XML övning/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.IO;
+
+namespace XML_övning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring variables
+            List<Pets> pets = new List<Pets>();
+
+            //asking for inputs to add into class Pets
+            Console.WriteLine("Petname");
+            string petName = Console.ReadLine();
+            Console.WriteLine("Pet description");
+            string petDesc = Console.ReadLine();
+            Console.WriteLine("pet speed");
+            string petSpeed = Console.ReadLine();
+            bool success = int.TryParse(petSpeed, out int petSpeedInt);
+            for (int i = 0; i < 2; i++)
+            {
+                pets.Add(new Pets() { petName = petName, petDesc = petDesc, petSpeed = petSpeedInt });
+            }
+
+
+            //Serializer
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Pets>));
+            FileStream myFile = File.Open("pets.xml", FileMode.OpenOrCreate);
+            serializer.Serialize(myFile, pets);
+
+            pets = (List<Pets>)serializer.Deserialize(myFile);
+
+
+            myFile.Close();
+        }
+    }
+}
